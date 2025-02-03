18d299cf38bc2488c1f8aacc1ec2b5fbef892194
Des Albert <des.albert@gmail.com>
2025-02-02 16:36:32 -0800

    Initial Commit

--- app/src/main/java/org/db/spellseek/ui/theme/Type.kt
+++ app/src/main/java/org/db/spellseek/ui/theme/Type.kt
@@ -15,14 +15,14 @@ val provider = GoogleFont.Provider(
 val bodyFontFamily = FontFamily(
     Font(
         googleFont = GoogleFont("Montserrat"),
-        fontProvider = org.db.spellseek.ui.theme.provider,
+        fontProvider = provider,
     )
 )
 
 val displayFontFamily = FontFamily(
     Font(
         googleFont = GoogleFont("Orbitron"),
-        fontProvider = org.db.spellseek.ui.theme.provider,
+        fontProvider = provider,
     )
 )
 
